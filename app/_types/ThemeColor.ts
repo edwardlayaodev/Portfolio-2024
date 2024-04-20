9398a7bb1668441d7362157a846c5fd307bfc69f
edwardlayaodev <edward.layao.dev@proton.me>
2024-04-20 10:49:51 +0800

    add types for daisyUI themes

---
+++ app/_types/ThemeColor.ts
@@ -0,0 +1,1 @@
+export type ThemeColor = 'neutral' | 'primary' | 'secondary' | 'info' | 'success' | 'warning' | 'error' | 'link' | 'outline'
---
+++ app/_types/ThemeSize.ts
@@ -0,0 +1,2 @@
+
+export type ThemeSize = 'xs' | 'sm' | 'md' | 'lg'
