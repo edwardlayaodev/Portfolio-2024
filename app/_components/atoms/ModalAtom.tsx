e011df75473e9abc0d2576acd86cbe92e7e2d72b
edwardlayaodev <edward.layao.dev@proton.me>
2024-04-25 09:44:18 +0800

    finish Modal component

--- app/_components/atoms/ModalAtom.tsx
+++ app/_components/atoms/ModalAtom.tsx
@@ -1,20 +1,23 @@
 "use client"
 
 import { ModalContext } from '@/app/_context/ModalContext'
-import React, { ReactNode, useContext, useEffect, useState } from 'react'
-import CardAtom from './CardAtom'
+import React, { useContext } from 'react'
 
-type Props = {
-  children?: ReactNode
-}
-
-export default function ModalAtom({ children }: Props) {
+export default function ModalAtom() {
   const ctx = useContext(ModalContext)
-  return (
-    <div className="fixed w-full bg-opacity-40 bg-neutral-500 min-h-screen flex flex-col justify-center items-center z-20">
-      <CardAtom twClassBody='w-3/4' card={{ title: "Title", desc: "description" }}></CardAtom>
 
-    </div>
+  function close(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    if ((e.target as HTMLDivElement).classList.contains("overlay")) {
+      ctx?.closeModal()
+    }
+  }
+
+  return (
+    (ctx?.isOpen &&
+      <div onClick={close} className="overlay fixed w-full bg-opacity-40 bg-neutral-500 min-h-screen flex flex-col justify-center items-center z-50">
+        {ctx?.content}
+      </div>
+    )
   )
 
 } 
--- app/_context/ModalContext.tsx
+++ app/_context/ModalContext.tsx
@@ -1,27 +1,36 @@
 "use client"
 
-import { createContext, useState, ReactNode } from "react";
+import React, { createContext, useState, ReactNode } from "react";
 
 type Props = {
   children: ReactNode
 }
 
-interface IModalInterface {
+interface IModal {
   isOpen: boolean
-  toggleModal: () => void
+  openModal: (content: ReactNode) => void
+  closeModal: () => void
+  content: ReactNode
 }
 
-export const ModalContext = createContext<IModalInterface | undefined>(undefined);
+export const ModalContext = createContext<IModal | undefined>(undefined);
 
 export const ModalProvider = ({ children }: Props) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [content, setContent] = useState<ReactNode | null>(null)
 
-  const toggleModal = () => {
-    setIsOpen(!isOpen)
+  function openModal(modalContent: ReactNode) {
+    setContent(modalContent)
+    setIsOpen(true)
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+    setContent(null)
   }
 
   return (
-    <ModalContext.Provider value={{ isOpen, toggleModal }}>
+    <ModalContext.Provider value={{ isOpen, content, openModal, closeModal }}>
       {children}
     </ModalContext.Provider>
   )
