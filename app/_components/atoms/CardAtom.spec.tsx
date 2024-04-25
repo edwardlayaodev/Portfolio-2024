e26e0be40a91bc586e983c32fb252fb3673c110c
edwardlayaodev <edward.layao.dev@proton.me>
2024-04-25 13:29:44 +0800

    add tests for uncovered lines in Card Atom

---
+++ app/_components/atoms/CardAtom.spec.tsx
@@ -0,0 +1,17 @@
+import '@testing-library/jest-dom'
+import { RenderResult, render } from '@testing-library/react'
+import CardAtom from './CardAtom';
+describe('Card Component', () => {
+  let component: RenderResult;
+  beforeEach(() => {
+    component = render(<CardAtom card={{ title: "title", desc: "desc", actions: [<button>OK</button>] }} />)
+  })
+
+  it('renders the title and description', () => {
+    const title = component.getByText('title')
+    const desc = component.getByText('desc')
+    expect(title).toBeInTheDocument()
+    expect(desc).toBeInTheDocument()
+  })
+
+})
