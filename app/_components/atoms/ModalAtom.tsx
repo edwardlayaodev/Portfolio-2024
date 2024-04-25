50c843896d7cfe3f70d461ad11e1576e9d6e3ffb
edwardlayaodev <edward.layao.dev@proton.me>
2024-04-25 13:28:32 +0800

    update modal component

--- app/_components/atoms/ModalAtom.tsx
+++ app/_components/atoms/ModalAtom.tsx
@@ -6,17 +6,16 @@ import React, { useContext } from 'react'
 export default function ModalAtom() {
   const ctx = useContext(ModalContext)
 
-  function close(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    if ((e.target as HTMLDivElement).classList.contains("overlay")) {
+  function close(e: React.MouseEvent<HTMLDialogElement, MouseEvent>) {
+    if ((e.target as HTMLDivElement).classList.contains('overlay'))
       ctx?.closeModal()
-    }
   }
 
   return (
     (ctx?.isOpen &&
-      <div onClick={close} className="overlay fixed w-full bg-opacity-40 bg-neutral-500 min-h-screen flex flex-col justify-center items-center z-50">
+      <dialog onClick={close} className="overlay fixed w-full bg-opacity-40 bg-neutral-500 min-h-screen flex flex-col justify-center items-center z-50">
         {ctx?.content}
-      </div>
+      </dialog>
     )
   )
 
