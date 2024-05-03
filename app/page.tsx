169ae774167816210a625b729380c8dbea347925
edwardlayaodev <edward.layao.dev@proton.me>
2024-05-03 13:02:39 +0800

    add portrait to hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import Image from "next/image";
+import TextInputAtom from "./_components/atoms/TextInputAtom";
+import TypewriterAtom from "./_components/atoms/TypewriterAtom";
+import PortraitAnimAtom from "./_components/atoms/PortraitAnimAtom";
 
 export default function Home() {
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="mockup-code text-left w-1/2">
-        <pre data-prefix="$" className="text-primary"><code>helloGreeting()</code></pre>
-        <pre data-prefix=">"><code>Greetings!, my name Is Edward Layao!</code></pre>
+    <div className="hero min-h-screen bg-base-100 text-base-content">
+      <div className="hero-content flex flex-col-reverse  justify-center items-center md:flex-row text-center">
+        <div className="w-1/2 border border-red-500 flex justify-center">
+          <PortraitAnimAtom></PortraitAnimAtom>
+        </div>
+        <div className="max-w-md w-1/2 border border-red-500">
+          <div className="chat chat-start">
+            <div className="chat-bubble bg-info text-info-content text-3xl w-[350px]"><TypewriterAtom text={`Hey there!, I'm Edward, your front-end developer from Dasma, Cavite!`}></TypewriterAtom></div>
+          </div>
+        </div>
       </div>
     </div>
+
+
   );
 }
