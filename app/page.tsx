601ce7679851080b2e2545359a87c5363cdf422c
Edward Layao <107934769+edwardlayaodev@users.noreply.github.com>
2024-05-08 13:54:37 +0800

    add PortraitAnim on Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,13 @@
-import Image from "next/image";
-import TextInputAtom from "./_components/atoms/TextInputAtom";
-import TypewriterAtom from "./_components/atoms/TypewriterAtom";
-import PortraitAnimAtom from "./_components/atoms/PortraitAnimAtom";
+"use client"
+import { useState } from "react";
+import PortraitAnimOrganism from "./_components/organisms/PortraitAnimOrganism";
 
 export default function Home() {
   return (
-    <div className="hero min-h-screen bg-base-100 text-base-content">
-      <div className="hero-content flex flex-col-reverse  justify-center items-center md:flex-row text-center">
-        <div className="w-1/2 border border-red-500 flex justify-center">
-          <PortraitAnimAtom></PortraitAnimAtom>
-        </div>
-        <div className="max-w-md w-1/2 border border-red-500">
-          <div className="chat chat-start">
-            <div className="chat-bubble bg-info text-info-content text-3xl w-[350px]"><TypewriterAtom text={`Hey there!, I'm Edward, your front-end developer from Dasma, Cavite!`}></TypewriterAtom></div>
-          </div>
-        </div>
+    <div className="bg-base-100 text-base-content">
+      <div className="flex flex-col-reverse justify-center items-center md:flex-row text-center pt-24">
+        <PortraitAnimOrganism />
       </div>
     </div>
-
-
   );
 }
