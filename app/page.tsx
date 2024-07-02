4ffb45ba2a2a2b484fb7145ed05a2e172c7edd33
Edward Layao <107934769+edwardlayaodev@users.noreply.github.com>
2024-07-02 14:40:21 +0800

    remove animation stuff (#6)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-"use client"
-import { useState } from "react";
-import PortraitAnimOrganism from "./_components/organisms/PortraitAnimOrganism";
+"use client";
 
 export default function Home() {
   return (
     <div className="bg-base-100 text-base-content">
-      <div className="flex flex-col-reverse justify-center items-center md:flex-row text-center pt-24">
-        <PortraitAnimOrganism />
-      </div>
+      <div className="flex flex-col-reverse justify-center items-center md:flex-row text-center pt-24"></div>
     </div>
   );
 }
